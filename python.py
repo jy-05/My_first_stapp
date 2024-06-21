3c95f4711c17047d6d3c582f7be1e31862ecd81b
jy-05 <jaeyynn@naver.com>
2024-06-21 09:04:47 +0900

    Add files via upload

---
+++ python.py
@@ -0,0 +1,76 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Ent.",
+    page_icon="📀",
+)
+
+st.write("# ⋆˚࿔  SM  𝜗𝜚˚⋆ ")
+
+st.sidebar.success("SM 소개")
+
+st.markdown(
+    """
+   대한민국의 대표 연예 기획사 SM """)
+
+st.title('1.SM')
+st.subheader('설립자')
+st.write('이수만')
+st.subheader('설립일')
+st.write('1995년 2월 14일')
+st.subheader('모기업')
+st.write('카카오')
+st.subheader('시가총액')
+st.write('1조 8,872억')
+st.subheader('본사 소재지')
+st.write('서울특별시 성동구 왕십리로 83-21 (성수동1가)')
+st.subheader('로고')
+st.image('https://i.namu.wiki/i/l3mNYCN4XqJYCHRigjTdJYG3vTVWCpBPn0t1_YiofnmNd4fdwgpDrW_BiHktQU17FFUnUHpyAloj1cAEsHEppXMlV6SLDW1OhZ76VlnIdRnEOnkqrULC51J8Pm2I9N82OOe1ekXWDX0_AQkBsNAQBw.svg')
+st.subheader("""1995 ~ 2017""")
+
+st.image('https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA5MTNfMjAx%2FMDAxNjk0NTk5NjEzOTk5.KPVv2fOb22cVTGMMMl7cFvwEcv3eAQnLBBOqXUzWaYcg.uRx8tUae3co0G2xviMeHT-EbkvOTYlKnxBqCUtAtlrQg.JPEG.yeiyei111%2F%25B4%25D9%25BF%25EE%25B7%25CE%25B5%25E5.jpg&type=sc960_832')
+st.subheader("""2017~""")
+st.title('SM의 역사')
+st.markdown("""
+    - '95 S.M. ENTERTAINMENT Co., Ltd. 설립
+    - '00 2000년 4월 27일 업계 최초로 코스닥시장 등록(IPO)
+    - '01 SM Entertainment JAPAN 설립
+    - '01Fandango KOREA 설립을 통해 국내 최초 엔터테인먼트 관련 온라인, 모바일 콘텐츠 서비스 시작
+    - '05 SM Entertainment China 지사 오픈
+    - '08 SM Amusement 설립
+    - '08 SM F&B 설립
+    - '08 SM Brand Marketing 설립
+    - '08 SM Entertainment USA Inc. 설립      
+    - '11 SM True Co., Ltd 설립     
+    - '12 SM Entertainment Beijing 설립     
+    - '12 드라마 제작 등 글로벌 영상 콘텐츠 사업을 영위하고 있으며, 관광, 레저, 여행 사업과 K-POP 콘텐츠를 결합해 K-컬쳐 비즈니스 모델 확산에 기여      
+    - '12 글로벌 공연 사업을 위해 홍콩에 본사를 두고 있는 자회사 Dream Maker Entertainment와 본격적으로 공연사업 시작
+    - '13 배우 매니지먼트 시작
+    - '14 예능 및 레이블사업 시작
+    - '14 모바일 가라오케 어플리케이션 런칭
+    - '15 모바일 플랫폼 비지니스 시작
+    - '15 엔터테인먼트 테마파크 사업 시작
+    - '16 F&B 사업 확장
+    - '17 광고사업으로의 확장
+    - '18 콘텐츠 및 라이프스타일 사업 확장
+    - '20 글로벌 온라인콘서트 브랜드 런칭
+    - '21 드라마, 예능, 뉴미디어 사업분야의 통합
+    - '22 메타버스 콘텐츠 제작기술 전문회사 설립
+    - '23 음악퍼블리싱 사업개시 """)
+st.title('3. 소개글')
+st.write("""
+    - SM Entertainment는 아시아를 넘어 글로벌 No.1을 지향합니다.
+    - SM Entertainment는 K-POP과 K-Culture의 미래를 설계합니다.
+    - SM Entertainment는 신뢰할 수 있는 책임 경영의 모범이 되겠습니다.   """)    
+st.title('4. 사내레이블')
+st.image('https://cdn2.smentertainment.com/wp-content/uploads/2024/02/label-sm-classics-2.png')
+st.subheader("""SM Classics""")
+st.markdown("""SM Classics는 SM 엔터테인먼트의 클래식 음악 전문 레이블입니다. K-Pop을 오케스트라 버전 및 다양한 편성의 클래식 음악으로 편곡하여 선보이고 있으며, 재즈, OST, 영화 음악 등 클래식과 관련된 여러 장르의 음악을 포괄적으로 다루고 있을 뿐만 아니라 아티스트들과의 다채로운 컬래버레이션을 통해 음악의 지평을 넓혀가고 있습니다.""") 
+st.image('https://cdn2.smentertainment.com/wp-content/uploads/2024/04/2404_scream-records-new.png')
+st.subheader("""ScreaM Records""")
+st.markdown("""ScreaM Records는 2016년 1월, ‘보고 듣는 퍼포먼스’에서 ‘함께 즐기는 퍼포먼스’로 확장하고 음악적 다변화를 꾀하고자 론칭한 SM Entertainment 산하 댄스 뮤직 레이블입니다. ScreaM Records는 음원 제작 및 유통, K-POP Remix 프로젝트 ‘iScreaM’, 소속 DJ/프로듀서 매니지먼트, 오프라인 이벤트 기획 및 운영 등의 사업을 기반으로 K-POP과 댄스 뮤직의 신선한 융합을 선도하고 있습니다.""")
+st.image('https://cdn2.smentertainment.com/wp-content/uploads/2024/05/KRUCIALIZE-logo-4.png')
+st.subheader("""KRUCIALIZE""")
+st.markdown("""KRUCIALIZE는 ARTIST와 KREW가 주체성을 가지고, 음악과 음악으로부터 파생되는 다양한 콘텐츠를 제작하는 SM Entertainment 산하 뮤직 레이블입니다. K-POP의 본질에 새로운 숨을 불어넣어 장르적 스펙트럼을 넓히고, 차별화된 독자적인 콘텐츠를 추구합니다. 더 다양한 Listener들이 KRUCIALIZE의 KREW가 될 수 있길 기다리고 있습니다.""")
+st.title('음악적 특징')
+st.write('SM만의 스타일인 SMP를 따른다. 따라서 SM은 아이돌에게 해당 아이돌 특유의 이미지를 주는 곡을 부르게 하여 마니아층(팬덤)을 양산하는 기획사이다. 특히 다른 기획사와의 가장 큰 차이점은 SM은 유로팝의 영향을 강하게 받는다. 그러다 보니 4대 기획사 중에서는 가장 독특한 음악적 색깔을 가졌다.')
